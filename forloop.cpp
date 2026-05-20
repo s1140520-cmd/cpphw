d7cff88c70616d3004870277edf901a67c0353fd
s1140520-cmd <s1140520@dmhs.kh.edu.tw>
2026-05-20 15:22:37 +0800

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum=0; 
+    for(int i = i; i <= 100; i++) {
+	   sum= sum  + i; 
+    }
+    cout << "1+2+3+...+100=" << sum; 
+}    retur  0;
